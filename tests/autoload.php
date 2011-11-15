2e524d146ed93e02d8441cdebfc2f42ae7924ac2
Przemek Sobstel <przemek@sobstel.org>
2011-11-15 20:50:22 +0100

    Setup test environment.

---
+++ tests/autoload.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../src/Execution/ClassLoader/ClassLoader.php';
+
+use Execution\ClassLoader\ClassLoader;
+
+$loader = new ClassLoader();
+$loader->register();
+
+require_once __DIR__.'/test_classes.php';
